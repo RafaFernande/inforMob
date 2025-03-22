6773cc60d54d02f0f4a9153a22a34030e670ac1e
RafaFernande <rafaelfaria833@gmail.com>
2025-03-22 19:28:36 -0300

    mudança de telas login e cadastro

--- informob/src/pages/Login/Login.jsx
+++ informob/src/pages/Login/Login.jsx
@@ -5,44 +5,106 @@ function Login(){
 
     const[email, setEmail] = useState('')
     const[senha, setSenha] = useState('')
-    const [bordaErroEmail, setBordaErroEmail] = useState(styles.input);
-    const [bordaErroSenha, setBordaErroSenha] = useState(styles.input);
-    const [mensagemErro, setMensagemErro] = useState('transparent')
+    const[senhaConfimar, setSenhaConfirmar] = useState('')
+    const [bordaEmail, setBordaEmail] = useState(styles.input)
+    const [bordaSenha, setBordaSenha] = useState(styles.input)
+    const [bordaSenhaConfirmar, setBordaSenhaConfirmar] = useState(styles.input)
+    const [mensagemCor, setMensagemCor] = useState('transparent')
+    const [loginDisplay, setLoginDisplay] = useState('flex')
+    const [cadastroDisplay, setCadastroDisplay] = useState('none')
 
-    function validar(){        
-        if(email == '' && senha != ''){
-            setBordaErroEmail(styles.inputErro)    
-            setBordaErroSenha(styles.input)
-            setMensagemErro('red')
-        }else if(senha == '' && email != ''){
-            setBordaErroSenha(styles.inputErro)
-            setBordaErroEmail(styles.input)    
-            setMensagemErro('red')
-        }else if(email == '' && senha == ''){
-            setBordaErroSenha(styles.inputErro)
-            setBordaErroEmail(styles.inputErro)    
-            setMensagemErro('red')
+    function validarLogin(){        
+        let erro = false;
+        if (email === '') {
+            setBordaEmail(styles.inputErro);
+           erro = true;
+        } else {
+            setBordaEmail(styles.input);
+        }
+     
+        if (senha === '') {
+            setBordaSenha(styles.inputErro);
+           erro = true;
+        } else {
+            setBordaSenha(styles.input);
+        }
+     
+        setMensagemCor(erro ? 'red' : 'transparent');
+     
+        if (!erro) {
+            mudarTela()
+           // Chamar API
+        }
+    }
+
+    function validarCadastro(){
+         let erro = false;
+        if (email === '') {
+            setBordaEmail(styles.inputErro);
+           erro = true;
+        } else {
+            setBordaEmail(styles.input);
+        }
+     
+        if (senha === '') {
+            setBordaSenha(styles.inputErro);
+           erro = true;
+        } else {
+            setBordaSenha(styles.input);
+        }
+        if (senhaConfimar === '') {
+            setBordaSenhaConfirmar(styles.inputErro);
+           erro = true;
+        } else {
+            setBordaSenhaConfirmar(styles.input);
+        }
+
+        setMensagemCor(erro ? 'red' : 'transparent');
+        if(!erro){
+            //Chamar API
+        }
+    }
+
+    function mudarTela(){
+        if(loginDisplay === 'flex'){
+            setLoginDisplay('none');
+           setCadastroDisplay('flex');
+           setMensagemCor('transparent')
+           setBordaEmail(styles.input)
+           setBordaSenha(styles.input)
+           setBordaSenhaConfirmar(styles.input)
         }else{
-            setBordaErroEmail(styles.input)    
-            setBordaErroSenha(styles.input)
-            setMensagemErro('transparent')
-            //API
+            setLoginDisplay('flex');
+           setCadastroDisplay('none');
+           setMensagemCor('transparent')
+           setBordaEmail(styles.input)
+           setBordaSenha(styles.input)
+           setBordaSenhaConfirmar(styles.input)
         }
     }
 
     return(
         <div id={styles.container}>
-            <div id={styles.login}>
+            <div id={styles.login} style={{display: loginDisplay}}>
                 <img src=""/>
                 <div id={styles.form}>
-                    <input className={bordaErroEmail} onChange={e => setEmail(e.target.value)} type="text" placeholder='e-mail'/>
-                    <input className={bordaErroSenha} onChange={e => setSenha(e.target.value)} type="password" placeholder='senha'/>
-                    <button onClick={validar}>Entrar</button>
+                    <input className={bordaEmail} onChange={e => setEmail(e.target.value)} type="text" placeholder='e-mail'/>
+                    <input className={bordaSenha} onChange={e => setSenha(e.target.value)} type="password" placeholder='senha'/>
+                    <button onClick={validarLogin}>Entrar</button>
+                    <button onClick={mudarTela}>Cadastrar</button>
                     <div id={styles.caixaAviso}>
-                        <p style={{color: mensagemErro}}>Preencha todos os campos!</p>
+                        <p style={{color: mensagemCor}}>Preencha todos os campos!</p>
                     </div>
                 </div>
-                <a href="">Esqueci minha senha</a>
+                <p style={{cursor: 'pointer'}}>Esqueci a minha senha</p>
+            </div>
+            <div id={styles.cadastro} style={{display: cadastroDisplay}}>
+                <input className={bordaEmail} onChange={e => setEmail(e.target.value)} type="text" placeholder='e-mail' />
+                <input className={bordaSenha} onChange={e => setSenha(e.target.value)} type="password" placeholder='senha'/>
+                <input className={bordaSenhaConfirmar} onChange={e => setSenhaConfirmar(e.target.value)} type="password" placeholder='repetir senha'/>
+                <button onClick={validarCadastro}>Cadastrar</button>
+                <p style={{color: mensagemCor}}>Preencha todos os campos!</p>
+                <p style={{cursor: 'pointer'}} onClick={mudarTela}>Já tenho uma conta</p>
             </div>
         </div>
     )
